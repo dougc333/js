e837a544d820833a77e71ef064d8b1402305c8ce
dougc333 <124989+dougc333@users.noreply.github.com>
2019-09-18 08:59:15 -0700

    add
     Author:    dougc333 <124989+dougc333@users.noreply.github.com>

---
+++ js_objects/classes.js
@@ -0,0 +1,30 @@
+//test classes
+//private data uses # well that isnt what vscode says.. 
+//says hash is invalid character! chrome ok! 
+//careful there is javascript, JavascriptReact, Typescript, Typescript React settins
+
+class Cat{
+  #name
+  _name
+  constructor(name){
+    this._name=name
+  }
+  set name(name){
+    this.#name=name
+  }
+  //if this isnt here the runtime creates this!
+  get name(){
+    console.log("calling get name")
+    return this.#name
+  }
+}
+
+const c = new Cat('fil') 
+c.name = "missing"  //problem is you can add to the runtime object wo error messages
+console.log(Object.getOwnPropertyNames(c))
+console.log(c) //object has a property _name set to fil
+console.log(c.name)
+c._name="b"
+console.log(c.name) //correct, uses underscore data member as generated get name
+console.log(c)
+

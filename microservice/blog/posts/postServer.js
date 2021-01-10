435d8fe8a52abf903b8f95fe951bd91108078748
dougc333 <dougc@stanfordalumni.org>
2021-01-10 09:46:48 -0800

    add

--- microservice/blog/posts/postServer.js
+++ microservice/blog/posts/postServer.js
@@ -1,13 +1,27 @@
-var express = require('express')
+const express = require('express')
+const bodyParser = require('body-parser');
 
-var portno = 3001;
-var app=express();
+const portno = 3001;
+const app=express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser);
+
+posts = {};
+id=0;
 
 app.get('/',function(request,response){
-  response.send("post server")
+
+  response.send("posts:", posts)
 });
+
+app.post('/',function(request,response){
+  posts[id] = request.body;
+  id++;
+  console.log("posts:",posts);
+  response.send(200)
+});
+
 
 var server=app.listen(portno, function(){
   var port = server.address().port;

82e6d8e42f5d241d77f20367089114f9d40cf620
dougc333 <dougc@stanfordalumni.org>
2021-02-15 11:40:33 -0800

    add

--- js_intro/reactive/app/sms.js
+++ js_intro/reactive/app/sms.js
@@ -1,9 +1,16 @@
+//https://www.twilio.com/blog/confirming-sms-message-delivery-with-rxjs-observables-node-js-and-twilio-programmable-sms
+
+
 import twilio from 'twilio'
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+console.log(client);
+
+
 client.messages.create({
   body:"asdfasdfasdfsa",
-  from: process.env.TWILIO_PHONE_NUMBER,
-  to: '+16507619455'
+  to: '+16503873796',
+  from: '+16507619455'
  }
 ).then(
   result=>{
